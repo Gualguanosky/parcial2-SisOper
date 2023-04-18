d88ce5ef64a71f370ff2f5d8b9cd14fca4d9319c
Gualguanosky <gualguan93@hotmail.com>
2023-04-17 22:09:48 -0500

    Update parcial2-punto2.c

--- parcial2-punto2.c
+++ parcial2-punto2.c
@@ -4,44 +4,22 @@
 
 //PUNTO 2
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
-
+//ASIGNO LAS CONSTANTES QUE SON EL NUMERO DE PARTICIONES QUE HAY EN EL PROGRAMA.
 #define MAX_PARTITIONS 100
 #define MAX_PROCESSES 100
-
+//creamos las tres funciones que vamos a utilizar  first_fit, best_fir, worst_fot
 void first_fit(int partitions[], int num_partitions, int processes[], int num_processes);
 void best_fit(int partitions[], int num_partitions, int processes[], int num_processes);
 void worst_fit(int partitions[], int num_partitions, int processes[], int num_processes);
 
 int main() {
+    //creamos las variables que utilizaremos
     int method, num_partitions, num_processes, i;
     int partitions[MAX_PARTITIONS], processes[MAX_PROCESSES];
 
-    // Leer entrada
-    scanf("%d", &method);
-    scanf("%d", &num_partitions);
-    for (i = 0; i < num_partitions; i++) {
-        scanf("%d", &partitions[i]);
-    }
-    scanf("%d", &num_processes);
-    for (i = 0; i < num_processes; i++) {#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_PARTITIONS 100
-#define MAX_PROCESSES 100
-
-void first_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-void best_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-void worst_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-
-int main() {
-    int method, num_partitions, num_processes, i;
-    int partitions[MAX_PARTITIONS], processes[MAX_PROCESSES];
-
-    // Leer entrada
+    // leemos las entradas
     scanf("%d", &method);
     scanf("%d", &num_partitions);
     for (i = 0; i < num_partitions; i++) {
@@ -52,7 +30,7 @@ int main() {
         scanf("%d", &processes[i]);
     }
 
-    // Ejecutar método de asignación de memoria
+    // Ejecutamo los métodos de asignación de ka memoria
     switch (method) {
         case 1:
             first_fit(partitions, num_partitions, processes, num_processes);
@@ -70,68 +48,12 @@ int main() {
 
     return 0;
 }
-
+// iniciamos la funcion first_ fit
 void first_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
     int i, j, partition_size, process_size;
     int partition_used[num_partitions];
-
-    for (i = 0; i < num_processes; i++) {
-        process_size = processes[i];
-        for (j = 0; j < num_partitions; j++) {
-            if (!partition_used[j]) {
-                partition_size = partitions[j];
-                if (process_size <= partition_size) {
-                    partition_used[j] = 1;
-                    printf("P%d -> %d se pone en %d, %d particiones.\n", i, process_size, partition_size, j+1);
-                    break;
-                }#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_PARTITIONS 100
-#define MAX_PROCESSES 100
-
-void first_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-void best_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-void worst_fit(int partitions[], int num_partitions, int processes[], int num_processes);
-
-int main() {
-    int method, num_partitions, num_processes, i;
-    int partitions[MAX_PARTITIONS], processes[MAX_PROCESSES];
-
-    // Leer entrada
-    scanf("%d", &method);
-    scanf("%d", &num_partitions);
-    for (i = 0; i < num_partitions; i++) {
-        scanf("%d", &partitions[i]);
-    }
-    scanf("%d", &num_processes);
-    for (i = 0; i < num_processes; i++) {
-        scanf("%d", &processes[i]);
-    }
-
-    // Ejecutar método de asignación de memoria
-    switch (method) {
-        case 1:
-            first_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        case 2:
-            best_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        case 3:
-            worst_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        default:
-            printf("Método no válido.\n");
-            break;
-    }
-
-    return 0;
-}
-
-void first_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
-    int i, j, partition_size, process_size;
-    int partition_used[num_partitions];
-
+    //utilizamos un arreglo de flags para para verificar si la particion ah sido asignada o no
+    // y comprobamos el tamaño de la misma
     for (i = 0; i < num_processes; i++) {
         process_size = processes[i];
         for (j = 0; j < num_partitions; j++) {
@@ -144,6 +66,7 @@ void first_fit(int partitions[], int num_partitions, int processes[], int num_pr
                 }
             }
         }
+        // condicion para mostar que no se puede asignar la particion.
         if (j == num_partitions) {
             printf("No se puede asignar %d a ninguna partición disponible.\n", process_size);
         }
@@ -174,106 +97,30 @@ void best_fit(int partitions[], int num_partitions, int processes[], int num_pro
     }
 }
 
-void worst_fit(int partitions[], int num_partitions
-            }
-        }
-        if (j == num_partitions) {
-            printf("No se puede asignar %d a ninguna partición disponible.\n", process_size);
-        }
-    }
-}
-
-void best_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
-    int i, j, partition_size, process_size, best_fit_index;
+void worst_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
+    int i, j, partition_size, process_size, worst_partition, worst_size;
     int partition_used[num_partitions];
+
+    for (i = 0; i < num_partitions; i++) {
+        partition_used[i] = 0;
+    }
 
     for (i = 0; i < num_processes; i++) {
         process_size = processes[i];
-        best_fit_index = -1;
+        worst_partition = -1;
+        worst_size = -1;
         for (j = 0; j < num_partitions; j++) {
             if (!partition_used[j]) {
                 partition_size = partitions[j];
-                if (process_size <= partition_size && (best_fit_index == -1 || partition_size < partitions[best_fit_index])) {
-                    best_fit_index = j;
+                if (process_size <= partition_size && partition_size > worst_size) {
+                    worst_partition = j;
+                    worst_size = partition_size;
                 }
             }
         }
-        if (best_fit_index == -1) {
+        if (worst_partition == -1) {
             printf("No se puede asignar %d a ninguna partición disponible.\n", process_size);
         } else {
-            partition_used[best_fit_index] = 1;
-            printf("P%d -> %d se pone en %d, %d particiones.\n", i, process_size, partitions[best_fit_index], best_fit_index+1);
+            partition_used[worst_partition] = 1;
+            printf("P%d -> %d se pone en %d, %d particiones.\n", i, process_size, partitions[worst_partition], worst_partition
         }
-    }
-}
-
-void worst_fit(int partitions[], int num_partitions
-        scanf("%d", &processes[i]);
-    }
-
-    // Ejecutar método de asignación de memoria
-    switch (method) {
-        case 1:
-            first_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        case 2:
-            best_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        case 3:
-            worst_fit(partitions, num_partitions, processes, num_processes);
-            break;
-        default:
-            printf("Método no válido.\n");
-            break;
-    }
-
-    return 0;
-}
-
-void first_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
-    int i, j, partition_size, process_size;
-    int partition_used[num_partitions];
-
-    for (i = 0; i < num_processes; i++) {
-        process_size = processes[i];
-        for (j = 0; j < num_partitions; j++) {
-            if (!partition_used[j]) {
-                partition_size = partitions[j];
-                if (process_size <= partition_size) {
-                    partition_used[j] = 1;
-                    printf("P%d -> %d se pone en %d, %d particiones.\n", i, process_size, partition_size, j+1);
-                    break;
-                }
-            }
-        }
-        if (j == num_partitions) {
-            printf("No se puede asignar %d a ninguna partición disponible.\n", process_size);
-        }
-    }
-}
-
-void best_fit(int partitions[], int num_partitions, int processes[], int num_processes) {
-    int i, j, partition_size, process_size, best_fit_index;
-    int partition_used[num_partitions];
-
-    for (i = 0; i < num_processes; i++) {
-        process_size = processes[i];
-        best_fit_index = -1;
-        for (j = 0; j < num_partitions; j++) {
-            if (!partition_used[j]) {
-                partition_size = partitions[j];
-                if (process_size <= partition_size && (best_fit_index == -1 || partition_size < partitions[best_fit_index])) {
-                    best_fit_index = j;
-                }
-            }
-        }
-        if (best_fit_index == -1) {
-            printf("No se puede asignar %d a ninguna partición disponible.\n", process_size);
-        } else {
-            partition_used[best_fit_index] = 1;
-            printf("P%d -> %d se pone en %d, %d particiones.\n", i, process_size, partitions[best_fit_index], best_fit_index+1);
-        }
-    }
-}
-
-void worst_fit(int partitions[], int num_partitions
